500ddd28fbeb9b5624724f15bd08b36d8e521491
Mariaanwar2004 <145532159+Mariaanwar2004@users.noreply.github.com>
2025-07-31 12:19:33 +0300

    --add remember me.

--- SyncSyntax/obj/Debug/net9.0/SyncSyntax.AssemblyInfo.cs
+++ SyncSyntax/obj/Debug/net9.0/SyncSyntax.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SyncSyntax")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e06ae1128ed9bb6b0b553a54f142224eacf320c7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5d0f69990f46cebce2c38f3668f2ac89d3c55d7")]
 [assembly: System.Reflection.AssemblyProductAttribute("SyncSyntax")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SyncSyntax")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
