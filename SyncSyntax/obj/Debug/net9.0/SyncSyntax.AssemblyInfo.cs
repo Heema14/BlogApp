8a7338387d5a708255071a29c971f8bfe311ef64
Mariaanwar2004 <145532159+Mariaanwar2004@users.noreply.github.com>
2026-01-16 11:14:54 +0200

    add loading delay before showing QR code

--- SyncSyntax/obj/Debug/net9.0/SyncSyntax.AssemblyInfo.cs
+++ SyncSyntax/obj/Debug/net9.0/SyncSyntax.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SyncSyntax")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e58c4cb3eeb208188c82ac4e0ff2466bd84ac7c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e43ee796e00bc83d77af14f9544277d10e090ad")]
 [assembly: System.Reflection.AssemblyProductAttribute("SyncSyntax")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SyncSyntax")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
