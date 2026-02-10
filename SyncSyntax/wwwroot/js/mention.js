23937ffb2becce359153274fceacc1622ef84511
Mariaanwar2004 <145532159+Mariaanwar2004@users.noreply.github.com>
2026-02-10 14:08:10 +0200

    feat: Add mentioning feature in posts

---
+++ SyncSyntax/wwwroot/js/mention.js
@@ -0,0 +1,30 @@
+﻿document.addEventListener('DOMContentLoaded', function () {
+    ClassicEditor
+        .create(document.querySelector('#editor'), {
+            mention: {
+                feeds: [
+                    {
+                        marker: '@',  
+                        feed: function (query) {
+                            const queryText = query.substring(1);  
+
+                            return fetch('/ContentCreator/Post/search?query=' + queryText)
+                                .then(response => response.json())
+                                .then(data => {
+                                    return data.map(user => {
+                                        return {
+                                            id: user.id,
+                                            text: user.username
+                                        };
+                                    });
+                                });
+                        }
+
+                    }
+                ]
+            }
+        })
+        .catch(error => {
+            console.error("Error initializing CKEditor:", error); 
+        });
+});
